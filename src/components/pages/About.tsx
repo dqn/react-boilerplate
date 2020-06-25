5690cbe4957bb19bc63fc6f0937c51a3e940b203
dqn <suyarizzz@gmail.com>
2020-06-25 20:39:40 +0900

    feat: add links

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const About: React.FC = () => {
   return (
     <>
       <h1 className="text-green-500 text-lg font-bold p-3">About</h1>
+      <Link to="/" className="ml-3">
+        Home
+      </Link>
     </>
   );
 };
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Home: React.FC = () => {
   return (
     <>
       <h1 className="text-blue-500 text-lg font-bold p-3">Home</h1>
+      <Link to="/about" className="ml-3">
+        About
+      </Link>
     </>
   );
 };
